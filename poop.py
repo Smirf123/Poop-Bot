6afe03eab8f85394a144bc515a4bf8754471d774
Smirf123 <smirf368@outlook.com>
2024-09-23 21:20:29 -0500

    Update poop.py

--- poop.py
+++ poop.py
@@ -1,7 +1,7 @@
 import discord
 from discord.ext import commands
 
-TOKEN = "ODc2NjIyODE0MDI1NzY0ODk1.G_adfc.FITOR2mMaHpc8kVZWairwMFZDf9isJKesfsJYE"
+TOKEN = ""
 activity = discord.Activity(type=discord.ActivityType.watching, name="for your messages")
 client = commands.Bot(command_prefix = "}")
 
@@ -14,5 +14,4 @@ async def on_message(message):
 
 
 client.run(TOKEN)
-ODc2NjIyODE0MDI1NzY0ODk1.GpiTM_.3JOU6etzcuB4fisIFJksSHpL_NqUIsnwKUSafI
-ODc2NjIyODE0MDI1NzY0ODk1.GZfkp_.o2JBlNzG8ME04snOGQXAT__Q28DdYTqUQ0lmGE
+
